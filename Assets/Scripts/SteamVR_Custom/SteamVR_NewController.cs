356741c0a81b81ae5839b380c28147a81ae2e6e6
Nick Fox-Gieg <nick@fox-gieg.com>
2017-02-08 08:31:57 -0500

    first

---
+++ Assets/Scripts/SteamVR_Custom/SteamVR_NewController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class SteamVR_NewController : MonoBehaviour {
+
+	public bool triggerPressed = false;
+	public bool padPressed = false;
+	public bool gripped = false;
+	public bool menuPressed = false;
+	public bool triggerDown = false;
+	public bool padDown = false;
+	public bool gripDown = false;
+	public bool menuDown = false;
+	public bool triggerUp = false;
+	public bool padUp = false;
+	public bool gripUp = false;
+	public bool menuUp = false;
+
+	private SteamVR_TrackedObject trackedObj;
+	//private bool blockTriggerButton = false;
+	//private bool blockGripButton = false;
+	//private bool blockPadButton = false;
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	//void FixedUpdate() {
+	void Update() {
+		var device = SteamVR_Controller.Input((int) trackedObj.index);
+
+		// ~ ~ ~ ~ ~
+
+		triggerDown = false;
+		padDown = false;
+		gripDown = false;
+		menuDown = false;
+		triggerUp = false;
+		padUp = false;
+		gripUp = false;
+		menuUp = false;
+
+		if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			triggerPressed = true;
+			triggerDown = true;
+		} else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) {
+			triggerPressed = false;
+			triggerUp = true;
+		}
+
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+			padPressed = true;
+			padDown = true;
+		} else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+			padPressed = false;
+			padUp = true;
+		}
+
+		if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Grip)) {
+			gripped = true;
+			gripDown = true;
+		} else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Grip)) {
+			gripped = false;
+			gripUp = true;
+		}
+
+		if (device.GetTouchDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+			menuPressed = true;
+			menuDown = true;
+		} else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+			menuPressed = false;
+			menuUp = true;
+		}
+
+		// ~ ~ ~ ~ ~
+
+		/*
+		triggerOneShot = false;
+		padOneShot = false;
+		gripOneShot = false;
+
+		if ((triggerPressed && !blockTriggerButton)) {
+			triggerOneShot = true;
+			blockTriggerButton = true;
+		} else if ((!triggerPressed && blockTriggerButton)) {
+			blockTriggerButton = false;
+		}
+
+		if ((padPressed && !blockPadButton)) {
+			padOneShot = true;
+			blockPadButton = true;
+		} else if ((!padPressed && blockPadButton)) {
+			blockPadButton = false;
+		}
+
+		if ((gripped && !blockGripButton)) {
+			gripOneShot = true;
+			blockGripButton = true;
+		} else if ((!gripped && blockGripButton)) {
+			blockGripButton = false;
+		}
+		*/
+	}
+
+}
---
+++ Assets/Scripts/SteamVR_Custom/SteamVR_NineMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamVR_NineMove : MonoBehaviour {
+
+	public SteamVR_NewController steamCtlMain;
+	public SteamVR_NewController steamCtlAlt;
+	public Transform world_root;
+
+	private Transform lastMain;
+	private Transform lastAlt;
+
+	private Vector3 deltaPos = Vector3.zero;
+	private Vector3 deltaRot = Vector3.zero;
+	private Vector3 deltaScale = Vector3.zero;
+
+	void Update() {
+		if (steamCtlMain.triggerDown) {
+			lastMain = steamCtlMain.transform;
+			lastAlt = steamCtlAlt.transform;
+		} else if (steamCtlMain.triggerPressed) {
+			deltaPos = ((steamCtlMain.transform.position - lastMain.position) + (steamCtlAlt.transform.position - lastAlt.position)) / 2f;
+			//deltaRot;
+			float getDeltaScale =  Vector3.Distance(steamCtlMain.transform.position, steamCtlAlt.transform.position) - Vector3.Distance(lastMain.position, lastAlt.position);
+			deltaScale = new Vector3(getDeltaScale, getDeltaScale, getDeltaScale);
+
+			world_root.Translate(deltaPos);
+			world_root.Rotate(deltaRot);
+			world_root.localScale += deltaScale;
+
+			lastMain = steamCtlMain.transform;
+			lastAlt = steamCtlAlt.transform;
+		}
+	}
+
+	float getAngle(Transform target) {
+		Vector3 relative  = transform.InverseTransformPoint(target.position);
+		float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+		return angle;
+	}
+
+}
