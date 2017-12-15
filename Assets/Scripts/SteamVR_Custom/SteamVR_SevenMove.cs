9eb56c1d2e4ebb815bc233a2808ae2a8234258d7
Nick Fox-Gieg <nick@fox-gieg.com>
2017-12-15 13:37:20 -0500

    translate works

--- Assets/Scripts/SteamVR_Custom/SteamVR_SevenMove.cs
+++ Assets/Scripts/SteamVR_Custom/SteamVR_SevenMove.cs
@@ -10,6 +10,9 @@ public class SteamVR_SevenMove : MonoBehaviour {
     public GameObject target;
     public GameObject center;
     public float minScale = 0.1f;
+    public float moveSpeed = 4f;
+    public enum TrackMode { AVG, MAIN, ALT};
+    public TrackMode trackMode = TrackMode.AVG;
 
     private Renderer centerRen;
     private Vector3 prevPosMain = Vector3.zero;
@@ -26,34 +29,12 @@ public class SteamVR_SevenMove : MonoBehaviour {
     private float deltaThreshold = 0.01f;
     private float scaleDelta = 5f;
 
-    private Matrix4x4 transformMatrix;
-    private Matrix4x4 cameraTransformMatrix;
-
-    public void updateTransformMatrix() {
-        transformMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
-    }
-
-    public void updateCameraTransformMatrix() {
-        cameraTransformMatrix = Matrix4x4.TRS(Camera.main.transform.position, Camera.main.transform.rotation, Camera.main.transform.localScale);
-    }
-
-    public Vector3 applyTransformMatrix(Vector3 p) {
-        return transformMatrix.MultiplyPoint3x4(p);
-    }
-
-    public Vector3 applyCameraTransformMatrix(Vector3 p) {
-        return cameraTransformMatrix.MultiplyPoint3x4(p);
-    }
-
-    void Awake() {
-        updateTransformMatrix();
-        updateCameraTransformMatrix();
-
+    private void Awake() {
         centerRen = center.GetComponent<Renderer>();
-        centerRen.enabled = false;
+        if (centerRen != null) centerRen.enabled = false;
     }
 
-    void Update() {
+    private void Update() {
         dist = (steamCtlMain.transform.position - steamCtlAlt.transform.position).magnitude;
         delta = dist - prevDist;
 
@@ -65,11 +46,17 @@ public class SteamVR_SevenMove : MonoBehaviour {
         centerPos = (steamCtlMain.transform.position + steamCtlAlt.transform.position) / 2f;
         center.transform.position = centerPos;
 
-        angle = getAngle(steamCtlMain.transform, centerPos);// (getAngle(steamCtlMain.transform, centerPos) + getAngle(steamCtlAlt.transform, centerPos)) / 2f;
+        if (trackMode == TrackMode.AVG) {
+            angle = (getAngle(steamCtlMain.transform, centerPos) + getAngle(steamCtlAlt.transform, centerPos)) / 2f;
+        } else if (trackMode == TrackMode.MAIN) {
+            angle = getAngle(steamCtlMain.transform, centerPos);
+        } else if (trackMode == TrackMode.ALT) {
+            angle = getAngle(steamCtlAlt.transform, centerPos);
+        }
         angleDelta = angle - prevAngle;
 
         if (steamCtlMain.gripped && steamCtlAlt.gripped) {
-            centerRen.enabled = true;
+            if (centerRen != null) centerRen.enabled = true;
 
             if (Mathf.Abs(delta) > Mathf.Abs(deltaThreshold * target.transform.localScale.x)) {
                 target.transform.localScale -= Vector3.one * Time.deltaTime * Mathf.Sign(delta) * scaleDelta;
@@ -78,10 +65,12 @@ public class SteamVR_SevenMove : MonoBehaviour {
 
             target.transform.Rotate(0f, -angleDelta, 0f);
 
-            target.transform.Translate(deltaPosAvg * 4f);
-
+            Vector3 move = new Vector3(-deltaPosAvg.x, deltaPosAvg.y, -deltaPosAvg.z) * moveSpeed;
+            Vector3 dir = target.transform.InverseTransformDirection(move);
+            Vector3 finalMove = new Vector3(move.x * dir.x, move.y, move.z * dir.z);
+            target.transform.Translate(dir);
         } else {
-            centerRen.enabled = false;
+            if (centerRen != null) centerRen.enabled = false;
         }
 
         prevPosMain = steamCtlMain.transform.position;
@@ -90,7 +79,7 @@ public class SteamVR_SevenMove : MonoBehaviour {
         prevAngle = angle;
     }
 
-    float getAngle(Transform t1, Vector3 v1) {
+    public float getAngle(Transform t1, Vector3 v1) {
         Vector3 relative = t1.InverseTransformPoint(v1);
         float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
         return angle;
